7ccd28ad5d51ba6e49f271f5671a9d6fa40af7a8
Lahiru illangasinghe <lahiruilangasinha@gmail.com>
2025-02-19 17:11:55 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,6 @@ import userRouter from "./route/signup.route.js";
 import employeeRouter from './route/employee.route.js';
 import productRouter from './route/product.route.js';
 import paymentRouter from './route/payment.route.js';
-import orderRouter from './route/order.route.js';
 
 dotenv.config();
 
@@ -34,7 +33,7 @@ app.use('/api/employees', employeeRouter);
 app.use('/api/products', productRouter);
 app.use('/api/payment', paymentRouter);
 
-app.use('/api/orders', orderRouter);
+
 
 
 
