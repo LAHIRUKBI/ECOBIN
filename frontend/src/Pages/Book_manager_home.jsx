5155acb6097152610123299f214a1bec334a1834
Lahiru illangasinghe <lahiruilangasinha@gmail.com>
2025-03-13 03:11:21 +0530

    updates add

--- frontend/src/Pages/Book_manager_home.jsx
+++ frontend/src/Pages/Book_manager_home.jsx
@@ -75,7 +75,7 @@ export default function Stockmanager_home() {
           {/* Overview Section */}
           <section className="text-center mb-16">
             <h1 className="text-4xl font-bold text-teal-600 mb-4">Welcome to the Service Manager Dashboard</h1>
-            <p className="text-lg text-gray-700">Manage services, add services, and add updates services.</p>
+            <p className="text-lg text-gray-700">Manage services, add services, and add updates.</p>
           </section>
 
           {/* Action Section */}
